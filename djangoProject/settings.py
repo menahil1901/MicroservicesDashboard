10023e6eafe2450139528652fbc7426d5d366a0e
menahil.hassan@vodafone.com <menahil.hassan@vodafone.com>
2024-03-21 23:38:25 +0000

    latest version of the app

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -15,6 +15,11 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_URL = 'dashboard/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'dashboard', 'static'),
+]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -120,7 +125,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/dashboard/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
